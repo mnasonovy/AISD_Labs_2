296b9f448c1e4063e0ec90cc1d5d475b4dfb0160
Михаил Насонов <mnasonovy@gmail.com>
2024-05-20 10:46:07 +0400

    task fix

--- Lab1/task.cc
+++ Lab1/task.cc
@@ -2,24 +2,17 @@
 #include <vector>
 #include "main.cc"
 
-bool Contains(const std::vector<int>& vec, int value) {
-    for (int num : vec) {
-        if (num == value) {
-            return true;
-        }
-    }
-    return false;
-}
-
-std::vector<int> FindDuplicates(const std::vector<int>& vec) {
+std::vector<int> findDuplicates(const std::vector<int>& vec) {
+    IntSet intSet;
     std::vector<int> duplicates;
 
-    for (size_t i = 0; i < vec.size(); ++i) {
-        for (size_t j = i + 1; j < vec.size(); ++j) {
-            if (vec[i] == vec[j] && !Contains(duplicates, vec[i])) {
-                duplicates.push_back(vec[i]);
-                break;
-            }
+    for (int num : vec) {
+
+        if (intSet.Contains(num)) {
+            duplicates.push_back(num);
+        }
+        else {
+            intSet.Insert(num);
         }
     }
 
@@ -27,18 +20,13 @@ std::vector<int> FindDuplicates(const std::vector<int>& vec) {
 }
 
 int main() {
-    IntSet intSet;
-    intSet.Insert(3);
-    intSet.Insert(2);
-    intSet.Insert(2);
-    intSet.Insert(4);
-    std::cout << "IntSet contains: ";
-    intSet.Print();
     std::vector<int> vec = { 3, 2, 2, 4 };
-    std::vector<int> duplicates = FindDuplicates(vec);
-    std::cout << "Found duplicates: ";
-    for (int i = 0; i < duplicates.size(); ++i) {
-        std::cout << duplicates[i] << " ";
+
+    std::vector<int> duplicates = findDuplicates(vec);
+
+    std::cout << "Duplicate elements: ";
+    for (int num : duplicates) {
+        std::cout << num << " ";
     }
     std::cout << std::endl;
 
